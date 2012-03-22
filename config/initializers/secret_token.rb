4d08ec2953e7cfaf67897218557c9503d311848c
blueplanet <erguolinge@gmail.com>
2012-03-23 07:15:19 +0900

    rspec install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = 'c4331a07e28a6dfd9ed368c94eeedbd132f200484a5f22852ff2326a70cb4fe94a4309901394f1316002408348e839cd145ab519af9a5beba7bc26e100c3bbb8'
