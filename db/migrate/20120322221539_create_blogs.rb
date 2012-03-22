803cdb081c331e46b9ecc189c97f801bbbb775a5
blueplanet <erguolinge@gmail.com>
2012-03-23 07:50:23 +0900

    blog name check added

--- db/migrate/20120322221539_create_blogs.rb
+++ db/migrate/20120322221539_create_blogs.rb
@@ -1,7 +1,7 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
-      t.string :name
+      t.string :name, :null => false
 
       t.timestamps
     end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,16 @@
+# encoding=utf-8
 require 'spec_helper'
 
-describe Blog do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Blog, " #name が設定されていない場合:"  do
+  before(:each) do
+    @blog = Blog.new
+  end
+
+  it "バリデーションに失敗すること" do
+    @blog.should_not be_valid
+  end
+
+  it ":name にエラーが設定されていること" do
+    @blog.should have(1).errors_on(:name)
+  end
 end
