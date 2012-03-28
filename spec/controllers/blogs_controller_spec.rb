a72324d8b698f44d006078459e8c826dab2a8e97
blueplanet <erguolinge@gmail.com>
2012-03-28 23:49:40 +0900

    controller test ok

--- spec/controllers/blogs_controller_spec.rb
+++ spec/controllers/blogs_controller_spec.rb
@@ -1,16 +1,28 @@
 #encoding:utf-8
 require 'spec_helper'
 
-describe BlogsController, "GET /show/:id" do
-  fixtures :blogs
+describe BlogsController do
+  describe "GET /show/:id" do
+    fixtures :blogs
 
-  it "リクエストは成功すること" do
-    get 'show', :id => blogs(:kakutani).id
-    response.should be_success
-  end
+    before do
+      @blog = blogs(:kakutani)
+      get 'show', :id => @blog.id
+    end
 
-  it ":idで指定したブログをロードすること" do
-    get 'show', :id => blogs(:kakutani).id
-    assigns[:blog].should == blogs(:kakutani)
+    it "リクエストは成功すること" do
+      response.should be_success
+    end
+
+    it ":idで指定したブログをロードすること" do
+      assigns[:blog].should == blogs(:kakutani)
+    end
+
+    it "blogs/showを描画することblogs" do
+      response.should render_template("blogs")
+    end
+    it "blogs/showを描画することblogs/show" do
+      response.should render_template("show")
+    end
   end
 end
